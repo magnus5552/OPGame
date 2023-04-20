dcc621220303dadada4255a4562eec2a3a2955a2
magnus5552 <isackov.stepa@yandex.ru>
2023-04-20 21:52:46 +0500

    Fucks

---
+++ Assets/Scripts/ButtonLogic.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonLogic : MonoBehaviour
+{
+    [SerializeField] 
+    public GameObject text;
+    [SerializeField] 
+    public GameObject bookOpened;
+
+    public void Start()
+    {
+        text.SetActive(false);
+        bookOpened.SetActive(false);
+    }
+
+    public void OnMouseEnter()
+    {
+        text.SetActive(true);
+    }
+
+    public void OnMouseExit()
+    {
+        text.SetActive(false);
+    }
+
+    private void OnMouseUp()
+    {
+        bookOpened.SetActive(true);
+    }
+}
