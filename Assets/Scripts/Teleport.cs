5d79c38dcd50435095111089b100e4fc2a7edc34
magnus5552 <isackov.stepa@yandex.ru>
2023-04-27 23:23:38 +0500

    Fixed sprites and add teleport

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    [SerializeField] 
+    public GameObject target;
+
+    private Teleport _targetTeleport;
+
+    private bool _isAbleToTeleport = true;
+
+    public void Start()
+    {
+        _targetTeleport = target.GetComponent<Teleport>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!_isAbleToTeleport) return;
+        
+        other.transform.position = target.transform.position;
+        _isAbleToTeleport = false;
+        _targetTeleport._isAbleToTeleport = false;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        _isAbleToTeleport = true;
+    }
+}
