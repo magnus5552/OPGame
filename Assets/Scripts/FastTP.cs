d8c83e0e35eac307d96310994f6e6dbabbc68eed
tacserus <A.V.Scheglevatov@urfu.me>
2023-05-12 03:57:58 +0500

    update3

---
+++ Assets/Scripts/FastTP.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FastTP : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject target;
+    [SerializeField]
+    private Transform camera, brother;
+    private FastTP _targetTeleport;
+
+    private bool _isAbleToTeleport = true;
+
+    public void Start()
+    {
+        _targetTeleport = target.GetComponent<FastTP>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!_isAbleToTeleport) return;
+
+        var side = (other.transform.position.x - camera.position.x >= 0) ? 0.5f : -0.5f;
+        var differenceC = other.transform.position - camera.position;
+        //var differenceB = other.transform.position - brother.position;
+        var differenceY = target.transform.position.y - other.transform.position.y;
+
+        other.transform.position = target.transform.position - new Vector3(side, differenceY, 0);
+        camera.position = target.transform.position - differenceC - new Vector3(side, differenceY, 0);
+        brother.position = target.transform.position; //- new Vector3(side, differenceY, 0) - differenceB
+
+        _isAbleToTeleport = false;
+        _targetTeleport._isAbleToTeleport = false;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        _isAbleToTeleport = true;
+    }
+}
