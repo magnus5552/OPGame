90a1a14173c9f9983e0af9f69ce57ee046e9c184
magnus5552 <isackov.stepa@yandex.ru>
2023-06-02 03:05:44 +0500

    Finally 3 level

--- Assets/Scripts/Barrier.cs
+++ Assets/Scripts/Barrier.cs
@@ -13,7 +13,8 @@ public class Barrier : MonoBehaviour
         var leftSide = gameObject.GetComponent<BoxCollider2D>().bounds.min.x;
         var otherLeftSide = other.bounds.min.x;
         var otherRigthSide = other.bounds.max.x;
-        if (otherLeftSide >= leftSide || otherRigthSide <= rightSide)
-            jone.position = Vector3.Lerp(jone.position, Vector3.left * force, Time.deltaTime / 4);
+        if ((otherLeftSide >= leftSide || otherRigthSide <= rightSide)
+            && (jone.position - other.transform.position).x < Screen.width / 2)
+            jone.position += Vector3.left * force; 
     }
 }
--- Assets/Scripts/JoneMovement.cs
+++ Assets/Scripts/JoneMovement.cs
@@ -14,32 +14,47 @@ public class JoneMovement : MonoBehaviour
     private Generation _generation;
     private ChangeRun _changeRun;
     private EndRun _endRun;
+    private AudioSource _audio;
+    private bool _isFirstEventFrame;
+
+    private float _johnHeight;
 
     private void Start()
     {
         _generation = street.GetComponent<Generation>();
         _changeRun = street.GetComponent<ChangeRun>();
         _endRun = eventSystem.GetComponent<EndRun>();
+        _audio = GetComponent<AudioSource>();
+        _isFirstEventFrame = true;
+        _johnHeight = GetComponent<SpriteRenderer>().size.y;
     }
 
     void FixedUpdate()
     {
         var eventRun = _changeRun.EventRun;
         if (!eventRun) return;
+
+        if (_isFirstEventFrame)
+        {
+            _audio.Play();
+            _isFirstEventFrame = false;
+        }
         
         var bottomSide = _generation.bottomSideGeneration;
         var topSide = _generation.topSideGeneration;
         
         jone.SetBool("isMove", true);
         var offsetY = Random.Range(-velocityY, velocityY);
-        if (transform.position.y >= topSide - 3f || transform.position.y <= bottomSide + 3f)
-            offsetY = -offsetY;
+        if (transform.position.y + _johnHeight + offsetY >= topSide || 
+            transform.position.y + _johnHeight + offsetY <= bottomSide)
+            offsetY = 0;
 
         transform.position += new Vector3(velocityX, offsetY, 0);
 
         if (player.position.x - transform.position.x <= distance)
         {
             _endRun.EndChase();
+            _audio.Stop();
         }
     }
 
