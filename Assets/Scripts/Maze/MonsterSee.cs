77e60cfc90c6b353d0f8baf27971720dfff6aad7
tacserus <A.V.Scheglevatov@urfu.me>
2023-06-01 22:14:13 +0500

    update8

--- Assets/Scripts/Maze/MonsterSee.cs
+++ Assets/Scripts/Maze/MonsterSee.cs
@@ -7,23 +7,21 @@ public class MonsterSee : MonoBehaviour
 {
     [SerializeField]
     private float maxDistance, velocity;
-    private List<Ray> rays;
-    private string[] tags;
-    private Vector3 direction;
-    
+    private Vector3[] directions;
+    private Ray ray;
+    private int index;
 
     // Start is called before the first frame update
     void Start()
     {
-        rays = new();
-        for (var x = -1; x < 2; x++)
-            for (var y = -1; y < 2; y++)
-            {
-                if (x * y != 0 || (x == 0 && y == 0))
-                    continue;
-                var ray = new Ray(transform.position, new Vector3(x, y, 0));
-                rays.Add(ray);
-            }
+        directions = new Vector3[]
+        {
+            new Vector3(1, 0, 0),
+            new Vector3(0, 1, 0),
+            new Vector3(-1, 0, 0),
+            new Vector3(0, -1, 0)
+        };
+        ray = new Ray(transform.position, directions[0]);
     }
 
     // Update is called once per frame
@@ -35,91 +33,22 @@ public class MonsterSee : MonoBehaviour
 
     private void MoveMonster()
     {
-        transform.position = Vector3.Lerp(transform.position, transform.position + direction * velocity, Time.deltaTime * 2);
+        
     }
 
     private void FindEmptiness()
     {
-        tags = new string[]
-        {
-            "Empty",
-            "Empty",
-            "Empty",
-            "Empty",
-        };
-        var foundPlayer = -1;
-        var countWalls = 0;
-        var directions = new Vector3[4];
+        ray.origin = transform.position;
+        var cross = Physics.Raycast(ray.origin, ray.direction, maxDistance);
 
-        for (var i = 0; i < rays.Count; i++)
+        if (!cross)
+            transform.position = Vector3.Lerp(transform.position, transform.position + ray.direction * velocity, Time.deltaTime * 2);
+        else
         {
-            int layerMask = 1 << LayerMask.NameToLayer("Wall");
-            var ray = rays[i];
-            ray.origin = transform.position;
-            var hit = Physics2D.Raycast(ray.origin, ray.direction, maxDistance, layerMask);
-            if (hit)
-            {
-                var gameObject = hit.collider.gameObject;
-                tags[i] = gameObject.tag;
-                switch (gameObject.tag)
-                {
-                    case "Wall":
-                        countWalls += 1;
-                        break;
-                    case "Monster":
-                        break;
-                    case "WhiteObject" or "BlackObject":
-                        foundPlayer = i;
-                        break;
-                }
-            }
-            else
-            {
-                directions[i] = rays[i].direction;
-            }    
+            ray.direction = directions[index];
+            index = (index + 1) % 4;
         }
-        if (directions[0] != new Vector3() || directions[3] != new Vector3())
-        {
-            direction = direction;
-        }
-        if (foundPlayer != -1)
-            direction = rays[foundPlayer].direction;
-        else if (countWalls == 3)
-            direction = (-1) * direction;
-        else if (countWalls == 2)
-        {
-            for (var i = 0; i < directions.Length; i++)
-            {
-                if (directions[i] == new Vector3())
-                    continue;
-
-                if (direction == directions[i])
-                    break;
-
-                if (directions[i] != (-1) * direction)
-                    direction = directions[i];
-            }
-        }
-        else if (countWalls == 1 || countWalls == 0)
-        {
-            FindDirection(directions);
-        }
-    }
-
-    private void FindDirection(Vector3[] directions)
-    {
-        var indexes = new List<int>();
-
-        for (var i = 0; i < directions.Length; i++)
-        {
-            if (directions[i] == (-1) * direction
-                || directions[i] == new Vector3())
-                continue;
-
-            indexes.Add(i);
-        }
-
-        var randomIndex = UnityEngine.Random.Range(0, indexes.Count);
-        direction = directions[indexes[randomIndex]];
+            
+            
     }
 }
